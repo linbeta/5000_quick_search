6f3d476f1c6672dc587ea932c635c5668922647a
Beta Lin <sb2828sb@gmail.com>
2021-10-25 14:17:44 +0800

    edit mobile display

--- main.py
+++ main.py
@@ -30,5 +30,5 @@ def show_result():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0",
+    app.run(debug=True, host="0.0.0.0",
             port=int(os.environ.get("PORT", 8080)))
